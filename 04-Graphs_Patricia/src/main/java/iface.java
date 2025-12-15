04ba6b3b9bb619652c74d774264260a97ee63ada
Patricia Camacho Rivallo <60811137+paticama@users.noreply.github.com>
2025-12-15 20:28:24 +0100

    Update iface.java

--- 04-Graphs_Patricia/src/main/java/iface.java
+++ 04-Graphs_Patricia/src/main/java/iface.java
@@ -1,7 +1,7 @@
 /**************************************************************************************************
  *
  * Class Name: iface
- * Authors name: Patricia Camacho Rivallo, Luis Dueñas Recuero, Rafael Camilo Rubio Quintero
+ * Authors name: PC, RCR, LD - Group 02
  * Creation date: 28/09/2025
  * Class version: 1.0
  * Class description: Adds an abstract method to read the data of a file
